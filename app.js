4e07daa1be5cb8fe81a75520d93faaf33c7b39f4
Matias1012 <mng3232@gmail.com>
2024-11-15 00:59:07 -0300

    arreglo

--- app.js
+++ app.js
@@ -33,6 +33,48 @@ function retornarProductoSugerido(edad) {
     return productoSugerido
 } 
 
-const edad = 24
-presentarUsuario("Hugo", edad);
-verificarEdad(edad);
+
+
+let NombreDelUsuario =  prompt("ingrese su nombre");
+let EdadDelUsuario =  Number(prompt("ingrese su edad"));
+let BebidaDelUsuario =  prompt("ingrese su bebida");
+
+let productos = [
+    { id: 1, nombre: "gaseosa", precio: 2000, stock: 5, categoria: "bebida sin alcohol"},
+    { id: 3, nombre: "agua", precio: 1000, stock: 7, categoria: "bebida sin alcohol"},
+    { id: 4, nombre: "jugo", precio: 1500, stock: 2, categoria: "bebida sin alcohol"},
+    { id: 6, nombre: "cerveza", precio: 3000, stock: 6, categoria: "bebida alcoholica"},
+    { id: 8, nombre: "whisky", precio: 9000, stock: 2, categoria: "bebida alcoholica"},
+    { id: 9, nombre: "fernet", precio: 5500, stock: 5, categoria: "bebida alcoholica"},
+];
+
+const ProductoBuscado = productos.find((producto) => { 
+    return  producto.id === 4
+});
+
+if (ProductoBuscado) {
+    console.log("Nombre del Producto " + ProductoBuscado.nombre)
+}
+
+const ProductoNoEncontrado = productos.find(producto => producto.id === 7)
+
+if (ProductoNoEncontrado === undefined) {
+    console.log("No se encuentra el Producto" )
+}
+
+let bebidasAlcoholicas = productos.filter(producto => producto.categoria === "bebida alcoholica")
+let bebidasSinAlcohol = productos.filter(producto => producto.categoria === "bebida sin alcohol")
+
+if (EdadDelUsuario >= 18) {
+    console.log(bebidasAlcoholicas)
+    
+    const bebidaBuscada = bebidasAlcoholicas.find(producto => producto.nombre === BebidaDelUsuario)
+    if (bebidaBuscada) {
+        console.log("LA bebida es "+ bebidaBuscada.nombre)
+    } else {
+        console.log("No se encontro la bebida")
+    }
+} else {
+    const ProductoNoEncontrado = productos.find(producto => producto.id === 7)
+    console.log(bebidasSinAlcohol)
+}
